43ef3f20cf82e1edd39f178210ce4d3f23f28276
Mikko Keinänen <mikko.keinanen@gmail.com>
2022-01-23 22:54:11 +0200

    Open file dialog

--- scanner-ui/scanner-ui.go
+++ scanner-ui/scanner-ui.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"log"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/storage"
 )
 
 type ScannerUi struct {
@@ -26,7 +30,21 @@ func (ui *ScannerUi) InitAndRun() {
 }
 
 func (ui *ScannerUi) makeMenu(a fyne.App, w fyne.Window) *fyne.MainMenu {
-	newItem := fyne.NewMenuItem("New", nil)
+	newItem := fyne.NewMenuItem("New", func() {
+		fd := dialog.NewFileOpen(func(reader fyne.URIReadCloser, err error) {
+			if err != nil {
+				dialog.ShowError(err, w)
+				return
+			}
+			if reader == nil {
+				log.Println("Cancelled")
+				return
+			}
+			defer reader.Close()
+		}, w)
+		fd.SetFilter(storage.NewExtensionFileFilter([]string{".png", ".jpg", ".jpeg"}))
+		fd.Show()
+	})
 
 	file := fyne.NewMenu("File", newItem)
 	return fyne.NewMainMenu(
