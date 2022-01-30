bfd9b50ac0b0a0e8e3ae00dc7b4d7a5ea244df98
Mikko Keinänen <mikko.keinanen@gmail.com>
2022-01-30 03:02:39 +0200

    shows image

--- scanner-ui/ui/image-widget.go
+++ scanner-ui/ui/image-widget.go
@@ -5,25 +5,31 @@ import (
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
 type imageWidgetRenderer struct {
-	image *canvas.Image
+	fyne.WidgetRenderer
+	//	image *canvas.Image
+	//	label *widget.Label
 	// widget  *ImageWidget
-	objects []fyne.CanvasObject
+	objects   []fyne.CanvasObject
+	container *fyne.Container
 }
 
 func (r *imageWidgetRenderer) Layout(s fyne.Size) {
-
+	fmt.Println("Layout")
 }
 
 func (r *imageWidgetRenderer) MinSize() fyne.Size {
-	return fyne.Size{}
+	return r.container.MinSize()
 }
 
 func (r *imageWidgetRenderer) Refresh() {
-
+	fmt.Println("Refresh")
+	r.container.Refresh()
+	//canvas.Refresh(r.container)
 }
 
 func (r *imageWidgetRenderer) Objects() []fyne.CanvasObject {
@@ -31,19 +37,19 @@ func (r *imageWidgetRenderer) Objects() []fyne.CanvasObject {
 }
 
 func (r *imageWidgetRenderer) Destroy() {
-
+	fmt.Println("Destroy")
 }
 
 type ImageWidget struct {
 	widget.BaseWidget
-	ImagePath string
-	OnTapped  func(event *fyne.PointEvent)
+	reader   fyne.URIReadCloser
+	OnTapped func(event *fyne.PointEvent)
 }
 
-func NewImageWidget(imagePath string) *ImageWidget {
+func NewImageWidget(reader fyne.URIReadCloser) *ImageWidget {
 	i := &ImageWidget{
-		ImagePath: imagePath,
-		OnTapped:  onTapped,
+		reader:   reader,
+		OnTapped: onTapped,
 	}
 	i.ExtendBaseWidget(i)
 	return i
@@ -56,11 +62,21 @@ func onTapped(event *fyne.PointEvent) {
 func (i *ImageWidget) CreateRenderer() fyne.WidgetRenderer {
 	i.ExtendBaseWidget(i)
 
-	image := canvas.NewImageFromFile(i.ImagePath)
-	objects := []fyne.CanvasObject{image}
+	//image := loadImage(i.reader)
+	image := canvas.NewImageFromReader(i.reader, "test")
+	image.Resize(fyne.NewSize(600, 600))
+	image.FillMode = canvas.ImageFillOriginal
+	// label := widget.NewLabel("Hello")
+	// label2 := widget.NewLabel("Hello")
+	container := container.NewHBox(image)
+	//container.Add(label2)
+	container.Resize(fyne.NewSize(500, 500))
+	objects := []fyne.CanvasObject{container}
 	r := &imageWidgetRenderer{
-		image:   image,
-		objects: objects,
+		//label:     label,
+		//image:     image,
+		objects:   objects,
+		container: container,
 	}
 	return r
 }
