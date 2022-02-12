a162394752c818bab47ef60a55e8580dbcacb058
Mikko Keinänen <mikko.keinanen@gmail.com>
2022-02-12 19:39:06 +0200

    Add file list

--- scanner-ui/ui/add-dialog.go
+++ scanner-ui/ui/add-dialog.go
@@ -1,6 +1,7 @@
 package ui
 
 import (
+	"errors"
 	"fmt"
 	"image"
 	_ "image/jpeg"
@@ -14,7 +15,6 @@ import (
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/layout"
-	"fyne.io/fyne/v2/storage"
 	"fyne.io/fyne/v2/widget"
 	"github.com/zorrokid/go-movies/scanner"
 	"github.com/zorrokid/go-movies/scanner-ui/util"
@@ -26,6 +26,8 @@ type AddDialog struct {
 	scanWindow     fyne.Window
 	app            fyne.App
 	text           *widget.Entry
+	fileListData   []fyne.URI
+	fileList       *widget.List
 }
 
 func NewAddDialog(w *fyne.Window, app fyne.App) *AddDialog {
@@ -42,19 +44,41 @@ func NewAddDialog(w *fyne.Window, app fyne.App) *AddDialog {
 	return dialog
 }
 
-func (d *AddDialog) setImage(reader fyne.URIReadCloser, err error) {
+func (d *AddDialog) readFiles(lu fyne.ListableURI, err error) {
 
 	if err != nil {
 		dialog.ShowError(err, *d.mainWindow)
 		return
 	}
-	if reader == nil {
-		log.Println("Cancelled")
+	uriList, err := lu.List()
+	if err != nil {
+		log.Fatal(err)
 		return
 	}
-	defer reader.Close()
 
-	filePath := reader.URI().Path()
+	uriListImages := make([]fyne.URI, 0)
+	for _, uri := range uriList {
+		fmt.Printf("file: %s, extension: %s\n", uri.Name(), uri.Extension())
+		if uri.Extension() == ".jpg" || uri.Extension() == ".jpeg" {
+			uriListImages = append(uriListImages, uri)
+		}
+	}
+
+	d.fileListData = uriListImages
+	d.fileList.Refresh()
+}
+
+func (d *AddDialog) setSelectedImage(i int) {
+	if i > len(d.fileListData)+1 {
+		dialog.ShowError(errors.New("selected index out of bounds"), d.scanWindow)
+	}
+	selectedImageURI := d.fileListData[i]
+	d.setImage(selectedImageURI)
+}
+
+func (d *AddDialog) setImage(uri fyne.URI) {
+
+	filePath := uri.Path()
 
 	if bbs, err := scanner.Scan(filePath, "fin"); err != nil {
 		log.Fatal(err)
@@ -94,13 +118,13 @@ func (d *AddDialog) setImage(reader fyne.URIReadCloser, err error) {
 
 func (d *AddDialog) createFileDialogButton() *widget.Button {
 	return widget.NewButton("Select image file (jpg or png)", func() {
-		fd := dialog.NewFileOpen(d.setImage, d.scanWindow)
-		fd.SetFilter(storage.NewExtensionFileFilter([]string{".png", ".jpg", ".jpeg"}))
+		fd := dialog.NewFolderOpen(d.readFiles, d.scanWindow)
 		fd.Show()
 	})
 }
 
 func (d *AddDialog) ShowDialog() {
+	d.fileList = d.createFileList()
 	content := container.New(layout.NewBorderLayout(nil, nil, nil, nil))
 	d.imageContainer = content
 	d.imageContainer.Resize(fyne.NewSize(1800, 1500))
@@ -114,47 +138,47 @@ func (d *AddDialog) ShowDialog() {
 	fieldsForm.Add(d.text)
 	fieldsForm.Add(clearBtn)
 
-	grid := container.NewAdaptiveGrid(3, d.createFileList(), scroll, fieldsForm)
+	grid := container.NewAdaptiveGrid(3, d.createFileListContainer(), scroll, fieldsForm)
 
 	d.scanWindow.SetContent(grid)
 	d.scanWindow.Show()
 }
 
-func (d *AddDialog) createFileList() fyne.CanvasObject {
+func (d *AddDialog) createFileList() *widget.List {
 
-	// icon := widget.NewIcon(nil)
-	// label := widget.NewLabel("Select An Item From The List")
-	// hbox := container.NewHBox(icon, label)
-
-	data := make([]string, 1000)
 	list := widget.NewList(
+		// length
 		func() int {
-			return len(data)
+			return len(d.fileListData)
 		},
+		// create item
 		func() fyne.CanvasObject {
 			return container.NewHBox(widget.NewIcon(theme.DocumentIcon()), widget.NewLabel("Template Object"))
 		},
+		// update item
 		func(id widget.ListItemID, item fyne.CanvasObject) {
-			item.(*fyne.Container).Objects[1].(*widget.Label).SetText(data[id])
+			item.(*fyne.Container).Objects[1].(*widget.Label).SetText(d.fileListData[id].Name())
 		},
 	)
 
-	list.OnSelected = func(id widget.ListItemID) {
-		// label.SetText(data[id])
-		// icon.SetResource(theme.DocumentIcon())
-	}
+	list.OnSelected = d.setSelectedImage
 	list.OnUnselected = func(id widget.ListItemID) {
 		// label.SetText("Select An Item From The List")
 		// icon.SetResource(nil)
 	}
 
+	list.Resize(fyne.NewSize(100, 100))
+
+	return list
+}
+
+func (d *AddDialog) createFileListContainer() fyne.CanvasObject {
+	d.fileListData = make([]fyne.URI, 0)
 	selectImageButton := d.createFileDialogButton()
 	selectImageButton.Resize(fyne.NewSize(100, 100))
 	content := container.New(layout.NewBorderLayout(selectImageButton, nil, nil, nil),
 		selectImageButton)
-	list.Resize(fyne.NewSize(100, 100))
-	content.Add(list)
-
+	content.Add(d.fileList)
 	return content
 }
 
